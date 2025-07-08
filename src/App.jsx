81a92da443517c0703b8ade26db0e149eb1dbec4
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-07-09 01:45:15 +0530

    Button component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 
 import ButtonGradient from "./assets/svg/ButtonGradient"
-
+import Button from "./components/Button"
 function App() {
  
   return (
     <>
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden ">
+<Button className="mt-10" href='#login' >
+  something
+</Button>
+    </div>
      <ButtonGradient/>
       
     </>
