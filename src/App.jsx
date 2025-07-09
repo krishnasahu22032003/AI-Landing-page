f534fa054bfa58fff774948208ca4c6c3e685536
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-07-09 14:43:33 +0530

    Tailwind utility classes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 
 import ButtonGradient from "./assets/svg/ButtonGradient"
 import Button from "./components/Button"
+import Header from "./components/Header"
 function App() {
  
   return (
     <>
     <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden ">
-<Button className="mt-10" href='#login' >
-  something
-</Button>
+<Header/>
     </div>
-     <ButtonGradient/>
-      
+  <ButtonGradient/>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+
+const Header = () => {
+  return (
+<div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm" >
+<div className="flex mt-5 items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4"  >
+    <a href="#hero" className="block w-[12rem] xl:mr-8 " >
+    <img src="/images/AI-logo.png"  className="w-[90px] h-[70px]"   alt="NeuroSpark" />
+</a>
+<nav className="fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+
+</nav>
+</div>
+</div>
+  )
+}
+
+export default Header
