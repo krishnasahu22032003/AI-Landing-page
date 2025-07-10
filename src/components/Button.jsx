452ff2db126cc1cea345da0b10ac1be23349c6c1
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-07-11 02:10:30 +0530

    Roadmap component created

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,12 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
 const Button = ({ className = "", href, onClick, children, px, white }) => {
-const classes = `
-  ${className}
-  button relative ${white ? "inline-flex" : ""} items-center text-center cursor-pointer justify-center h-11 transition-colors hover:text-color-1
-  ${px || "px-7"} ${white ? "text-n8" : "text-n1"}
-`;
+  const classes = `
+    ${className}
+    button relative inline-flex items-center justify-center text-center cursor-pointer h-11 transition-colors hover:text-color-1
+    ${px || "px-7"} ${white ? "text-n8" : "text-n1"}
+  `;
+
   const spanClasses = "relative z-10";
 
   const renderButton = () => (
