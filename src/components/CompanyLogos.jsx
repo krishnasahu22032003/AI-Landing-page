65b6f07b172e4df0a7700a343a73a8d3f17870f8
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-07-11 02:38:57 +0530

    CompanyLogo component content changed

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -4,7 +4,7 @@ const CompanyLogos = ({ className }) => {
   return (
     <div className={className}>
       <h5 className="tagline mb-6 text-center text-n1/50">
-        Helping people create beautiful content at
+       Fueling creativity through powerful AI innovation
       </h5>
       <ul className="flex">
         {companyLogos.map((logo, index) => (
