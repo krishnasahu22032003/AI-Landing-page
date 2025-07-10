6b27a8753b4ad1f52b8cb9de805b8a1c5f30ce4d
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-07-10 18:24:27 +0530

    Benefits section added

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -13,10 +13,10 @@ const Benefits = () => {
           title="Neurospark: Ignite Intelligent Conversations."
         />
 
-        <div className="flex flex-wrap gap-10 mb-10">
+        <div className="flex flex-wrap justify-center gap-10 mb-10">
           {benefits.map((item) => (
             <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              className="block relative p-1 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
               style={{
                 backgroundImage: `url(${item.backgroundUrl})`,
               }}
@@ -33,7 +33,7 @@ const Benefits = () => {
                     alt={item.title}
                   />
                   <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
-                    Explore more
+                   Unlock Potential
                   </p>
                   <Arrow />
                 </div>
